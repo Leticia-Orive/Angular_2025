cd1fad320f88054c720b8d30487f23507886fc5c
Leticia Orive <leticia.orive@accenture.com>
2025-03-05 13:53:02 +0100

    Tarea - Separar template de la lógica

--- 02-bases/src/app/pages/counter/counter.component.ts
+++ 02-bases/src/app/pages/counter/counter.component.ts
@@ -2,9 +2,9 @@ import { Component } from "@angular/core";
 
 
 @Component({
-  template: `
-  <h1>Counter: {{counter}}</h1><!--Podemos poner cualquier expresion en blanco nos va a dar error-->
-  <button (click)="increaseBy(1)">+1</button>`,
+
+  templateUrl: './counter.component.html',
+  styleUrls: ['./counter.component.css']
 })
 
 
@@ -14,6 +14,10 @@ export class CounterComponent {
   // Metodo
   increaseBy(value: number){
     this.counter += value;
+  }
+
+  resetCounter(){
+    this.counter = 10;
   }
 
 }
