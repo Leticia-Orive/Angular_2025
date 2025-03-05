882c1bb1d862c560c9adb82ed03b27f6e66745bd
Leticia Orive <leticia.orive@accenture.com>
2025-03-05 11:43:13 +0100

    generico

--- 01-typescript-intro/src/topics/09-generics.ts
+++ 01-typescript-intro/src/topics/09-generics.ts
@@ -1,13 +1,13 @@
 
 
-export function whatsMyType<T>( argument: T): T{
+export function whatsMyType<T>( argument: T): T {
     return argument ;
 }
 
-let amIString = whatsMyType('Hello World');
-let amINumber = whatsMyType(100);
-let amIArray = whatsMyType([1,2,3,4,5]);
+let amIString = whatsMyType<string>('Hello World');
+let amINumber = whatsMyType<number>(100);
+let amIArray = whatsMyType<number[]>([1,2,3,4,5]);
 
-console.log(amIString.split2(''));
-console.log(amIString.split2(''));
-console.log(amIString.split2(''));
+console.log(amIString.split(''));
+console.log(amINumber.toFixed());
+console.log(amIArray.join('-'));
