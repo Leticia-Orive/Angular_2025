609e308644c6bb9078b1a85a89a8c19b63beafea
Leticia Orive <leticia.orive@accenture.com>
2025-03-05 10:01:39 +0100

    Priorizar composición sobre herencia

--- 01-typescript-intro/src/topics/08-classes.ts
+++ 01-typescript-intro/src/topics/08-classes.ts
@@ -12,14 +12,16 @@ export class Person {
  */
 // esta es la forma mas corta de ponerlo
     constructor(
-        public name: string,
+        //public name: string,
+        public firstName: string,
+        public lastName: string,
         private address: string = 'No Address'
     ) {}
     
 
 }
 
-export class Hero extends Person {
+/*export class Hero extends Person {
     constructor(
         public alterEgo: string,
         public age: number,
@@ -28,7 +30,7 @@ export class Hero extends Person {
         super( realName, 'new york');
     }
     
-}
+}*/
     
 //? significa que es opcional
 /**Esta es otra forma que no esta mal pero mas larga 
@@ -41,9 +43,26 @@ export class Hero extends Person {
         this.address = address;
     }*/ 
 
+        export class Hero  {
+            //creamos una nueva propiedad que es de tipo Person
+            //public person: Person; 
+            constructor(
+                public alterEgo: string,
+                public age: number,
+                public realName: string,
+                public person: Person,
+            ) {
+                /** Envede crear la instancia en este punto que se puede crear lo voy a crear aqui 
+                this.person = new Person(realName);*/
+                
+            }
+            
+        }
+ 
+const leticia = new Person('Leticia','Orive', 'Calle Falsa 123');
 
 
-const ironman = new Hero('Ironman',45, 'Tony');
+const ironman = new Hero('Ironman',45, 'Tony', leticia);
 console.log(ironman);
 
 /** Se puede poner tambien asi
