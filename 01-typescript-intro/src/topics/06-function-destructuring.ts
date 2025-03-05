171f593abd1ffebda441871688047c4fa8b0c8e2
Leticia Orive <leticia.orive@accenture.com>
2025-03-05 08:34:18 +0100

    Resolución de la tarea - Desestructuración

--- 01-typescript-intro/src/topics/06-function-destructuring.ts
+++ 01-typescript-intro/src/topics/06-function-destructuring.ts
@@ -19,14 +19,24 @@ interface TaxCalculationOptions {
     products: Product[];
 }
 
-function taxCalculation(options: TaxCalculationOptions): number[] {
+function taxCalculation(options: TaxCalculationOptions): [number, number] {
+    //function taxCalculation({tax, products}: TaxCalculationOptions): [number, number] {
+    //function taxCalculation(options: TaxCalculationOptions): [number, number] {
+    const {tax, products} = options;
     //implementacion
     let total = 0;
-    options.products.forEach(product => {
-        //son lo mismo total = total + product.price;
+    /*options.products.forEach(product => {
+        son lo mismo total = total + product.price;
         total += product.price;
+    });*/
+    //Destructuracion de tareas
+    //options.products.forEach(({price}) => {
+        products.forEach(({price}) => {    
+        total += price;
     });
-    return[total, total * options.tax];
+    //return[total, total * options.tax];
+    return[total, total * tax];
+    
 
 }
 
@@ -34,12 +44,20 @@ function taxCalculation(options: TaxCalculationOptions): number[] {
 const shoppingCart = [phone, tablet];
 const tax = 0.16;
 
-const result = taxCalculation({
-    tax, 
+//Para que tax no salgo con error se le puede poner un valor por defecto
+//const tax = 0.16;
+/** 
+const [total, tax] = taxCalculation({
+    tax: 0.15, 
+    products: shoppingCart
+}); */
+
+const [total, taxTotal] = taxCalculation({
+    tax: tax, 
     products: shoppingCart
 });
 
-console.log('Total', result[0]);
-console.log('Tax', result[1]);
+console.log('Total', total);
+console.log('Tax', tax);
 
 export {};
