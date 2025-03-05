da1f03f3e312b8d6fde47da777ed24a6f6858c30
Leticia Orive <leticia.orive@accenture.com>
2025-03-05 14:05:40 +0100

    señales

--- 02-bases/src/app/pages/counter/counter.component.ts
+++ 02-bases/src/app/pages/counter/counter.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, signal } from "@angular/core";
 
 
 @Component({
@@ -10,14 +10,21 @@ import { Component } from "@angular/core";
 
 export class CounterComponent {
   counter = 10;
+  counterSignal = signal(10);
 
   // Metodo
   increaseBy(value: number){
     this.counter += value;
+    /**No es la formas mas apropiada
+     * this.counterSignal.set(this.counterSignal() + value);
+     */
+
+    this.counterSignal.update((current) => current + value);//nos da la opcion de mandar
   }
 
   resetCounter(){
-    this.counter = 10;
+    this.counter = 0;
+    this.counterSignal.set(0); //el set ignora el valor
   }
 
 }
