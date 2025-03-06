d82b91cb4bf2f57bbb934a3ec8476ed378d97a88
Leticia Orive <leticia.orive@accenture.com>
2025-03-06 10:18:07 +0100

    reparacion de paginas

---
+++ 02-bases/src/app/pages/dragonball/dragonball-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dragonball-page',
+  templateUrl: './dragonball-page.component.html',
+  styleUrls: ['./dragonball-page.component.css']
+})
+export class DragonballPageComponent {
+  // Component logic here
+}
