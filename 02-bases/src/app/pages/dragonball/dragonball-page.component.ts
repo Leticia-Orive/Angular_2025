16def6c64ebbb9dc2a8d6b162c4e57163ff1e830
Leticia Orive <leticia.orive@accenture.com>
2025-03-06 12:48:25 +0100

    solucion de tarea

--- 02-bases/src/app/pages/dragonball/dragonball-page.component.ts
+++ 02-bases/src/app/pages/dragonball/dragonball-page.component.ts
@@ -34,6 +34,27 @@ export class DragonballPageComponent {
     }
   })
    */
+  addCharacter() {
+    //console.log(this.name(), this.power());
+    if (!this.name() || !this.power() || this.power() <= 0) {
+      return;
+    }
+
+    const newCharacter: Character = {
+      id: this.characters().length + 1,
+      name: this.name(),
+      power: this.power(),
+    };
+
+    this.characters.update((list) => [...list, newCharacter]);
+    this.resetFields();
+  }
+
+  resetFields() {
+    this.name.set('');
+    this.power.set(0);
+  }
+  }
 
 
-}
+
