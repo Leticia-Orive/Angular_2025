cdd538571a9fbfae8ada03b4aba7d3450a5b3c8d
Leticia Orive <leticia.orive@accenture.com>
2025-03-06 16:30:25 +0100

    correcion de errores

--- 02-bases/src/app/app.routes.ts
+++ 02-bases/src/app/app.routes.ts
@@ -1,28 +1,35 @@
 import { Routes } from '@angular/router';
 import { CounterPageComponent } from './pages/counter/counter-page.component';
 import { HeroPageComponent } from './pages/hero/hero-page.component';
-import { DragonballPageComponent } from './pages/dragonball/dragonball-page.component';
+
+import { DragonballComponent } from './pages/dragonball/dragonball.component';
 import { DragonballSuperPageComponent } from './pages/dragonball-super/dragonball-super-page.component';
+
 
 export const routes: Routes = [
   {
     path: '',
-    component: CounterPageComponent,
+    component: CounterPageComponent
+
   },
   {
     path: 'hero',
-    component: HeroPageComponent,
+    component: HeroPageComponent
   },
   {
     path: 'dragonball',
-    component: DragonballPageComponent,
+    component: DragonballComponent
   },
   {
     path: 'dragonball-super',
-    component: DragonballSuperPageComponent,
+    component: DragonballSuperPageComponent
   },
-  {
-    path: '**',
-    redirectTo: '',
-  },
+
+
+
+  //redireccionar a la pagina principal si no se encuentra la ruta
+{
+  path: '**',
+  redirectTo: ''
+}
 ];
--- 02-bases/src/app/pages/dragonball/dragonball.component.ts
+++ 02-bases/src/app/pages/dragonball/dragonball.component.ts
@@ -1,7 +1,5 @@
-
-import { Component,  signal } from '@angular/core';
-import { CharacterListComponent } from "../../components/dragonball/character-list/character-list.component";
-import { CharacterAddComponent } from '../../components/dragonball/character-add/character-add.component';
+import { NgClass } from '@angular/common';
+import { Component, computed, signal } from '@angular/core';
 
 //1. declarar la interface de personajes de dragonball
 interface Character {
@@ -11,14 +9,14 @@ interface Character {
 }
 
 @Component({
-  selector: 'dragonball-super',
-  imports: [CharacterListComponent, CharacterAddComponent],
-  templateUrl: './dragonball-super-page.component.html',
-  styleUrl: './dragonball-super-page.component.css'
+  selector: 'app-dragonball',
+  imports: [],
+  templateUrl: './dragonball.component.html',
+  styleUrl: './dragonball.component.css'
 })
 
 
-export class DragonballSuperPageComponent {
+export class DragonballComponent {
 
   //añadir signal a name y power
   name = signal('');
@@ -27,8 +25,15 @@ export class DragonballSuperPageComponent {
   //2- inicializar el array de personajes de dragonball interface con signal
   characters = signal<Character[]>([
     { id: 1, name: 'Goku', power: 15000 },
-    { id: 2, name: 'Vegeta', power: 12000 },
-
+    // { id: 2, name: 'Vegeta', power: 12000 },
+    // { id: 3, name: 'Gohan', power: 10000 },
+    // { id: 4, name: 'Piccolo', power: 8000 },
+    // { id: 5, name: 'Trunks', power: 9000 },
+    // { id: 6, name: 'Goten', power: 8000 },
+    // { id: 7, name: 'Krillin', power: 5000 },
+    // { id: 8, name: 'Yamcha', power: 500 },  //añadire,os un if a Yamcha
+    // { id: 9, name: 'Tien', power: 400 },
+    // { id: 10, name: 'Chaoz', power: 2000 },
 
   ]);
 
