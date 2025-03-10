4a9f65b118580093fbd7bdee53c33a59b89ae8a4
Leticia Orive <leticia.orive@accenture.com>
2025-03-10 15:52:48 +0100

    arreglar la pagina

--- 03-gifs-app/src/app/gifs/pages/search-page/search-page.component.ts
+++
@@ -1,9 +0,0 @@
-import {  Component } from '@angular/core';
-
-@Component({
-  selector: 'app-search-page',
-  imports: [],
-  templateUrl: './search-page.component.html',
-
-})
-export default class SearchPageComponent { }
--- 03-gifs-app/src/app/gifs/pages/trending-page/trending-page.component.ts
+++
@@ -1,9 +0,0 @@
-import {  Component } from '@angular/core';
-
-@Component({
-  selector: 'app-trending-page',
-  imports: [],
-  templateUrl: './trending-page.component.html',
-
-})
-export default class TrendingPageComponent { }
