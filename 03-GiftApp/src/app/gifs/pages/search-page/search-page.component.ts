c0481495a80a2c822ead15233e97c65718a97a9c
Leticia Orive <leticia.orive@accenture.com>
2025-03-11 13:26:29 +0100

    diseño de pantalla 'buscador de gifs'

--- 03-GiftApp/src/app/gifs/pages/search-page/search-page.component.ts
+++ 03-GiftApp/src/app/gifs/pages/search-page/search-page.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { GifListComponent } from '../../components/gif-list/gif-list.component';
+import { GifService } from '../../services/gifs.service';
 
 @Component({
   selector: 'app-search-page',
+  imports: [GifListComponent],
   templateUrl: './search-page.component.html',
   styleUrls: ['./search-page.component.css']
 })
-export  default class SearchPageComponent implements OnInit {
+export  default class SearchPageComponent  {
+//Inyectamos el servicio
+gifService = inject(GifService)
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+//Crear un metodo
+onSearch(query: string){
+  //console.log({query})
+  this.gifService.searchGifs(query);
+}
 
 }
--- 03-GiftApp/src/app/gifs/services/gifs.service.ts
+++ 03-GiftApp/src/app/gifs/services/gifs.service.ts
@@ -48,4 +48,23 @@ export class GifService {
 
   }
 
-}
+  //Creamos otro metodo
+  searchGifs(query: string){
+    this.http
+      .get<GiphyResponse>(`${environment.giphyUrl}/gifs/search`, {
+        params: {
+          api_key: environment.giphyApiKey,
+          limit: 20,
+          q: query,
+        },
+      })
+      .subscribe((resp) => {
+        const gifs = GifMapper.mapGiphyItemsToGifArray(resp.data);
+
+        console.log({ search: gifs });
+      });
+  }
+
+  }
+
+
